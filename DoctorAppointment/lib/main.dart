0cde2297fb13c62a4ea567755604e8102f824559
NadaMarmouch <76530614+NadaMarmouch@users.noreply.github.com>
2021-12-05 23:37:49 +0200

    home page

--- DoctorAppointment/lib/main.dart
+++ DoctorAppointment/lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:doctor_appointment/screens/Signup.dart';
 import 'package:flutter/material.dart';
+import 'package:doctor_appointment/screens/home_page.dart';
 import 'screens/Login.dart';
 import 'screens/Signup.dart';
+import 'screens/Firstscreen.dart';
 
 void main() {
   runApp( MaterialApp(
-     home: SignupPage(),
+   //  home: SignupPage(),
     title: 'Doctor Appointment',
-   // home: FirstScreen(),
+  // home: FirstScreen(),
+    home: HomePage(),
     
   ));
 }
---
+++ DoctorAppointment/lib/screens/home_page.dart
@@ -0,0 +1,253 @@
+import 'package:flutter/material.dart';
+import 'package:doctor_appointment/screens/doctor_details.dart';
+
+
+class HomePage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => HomePageState();
+}
+
+class HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return initScreen();
+  }
+
+  Widget initScreen() {
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Color(0xFF00abff),
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Color(0xFF00abff),
+        centerTitle: true,
+        leading: Icon(
+          Icons.menu,
+          color: Colors.white,
+        ),
+        actions: [
+          GestureDetector(
+            child: Container(
+              margin: EdgeInsets.only(right: 10),
+              child: Icon(
+                Icons.notifications_rounded,
+                color: Colors.white,
+              ),
+            ),
+          ),
+         
+        ],
+      ),
+      body: Container(
+        decoration: BoxDecoration(
+            color: Colors.grey[100],
+            borderRadius: BorderRadius.only(
+                topRight: Radius.circular(30), topLeft: Radius.circular(30))),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 20, left: 20),
+              child: Text(
+                "Hi,......",
+                style: TextStyle(
+                  color: Color(0xff363636),
+                  fontSize: 25,
+                  fontFamily: 'Roboto',
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 5, left: 20),
+              child: Text(
+                "Welcome Back",
+                style: TextStyle(
+                  color: Color(0xff363636),
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Roboto',
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 25, left: 20, right: 20),
+              width: size.width,
+              height: 60,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(5),
+                boxShadow: [
+                  BoxShadow(
+                    color: Color(0xFF00abff),
+                    offset: Offset(0, 10),
+                    blurRadius: 15,
+                    spreadRadius: 0,
+                  ),
+                ],
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    flex: 5,
+                    child: Container(
+                      margin: EdgeInsets.only(left: 10, right: 10),
+                      child: TextField(
+                        maxLines: 1,
+                        autofocus: false,
+                        style: TextStyle(color: Color(0xFF00abff), fontSize: 20),
+                        decoration: InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'Search..',
+                        ),
+                        cursorColor: Color(0xFF00abff),
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Color(0xFF0077c6), //search color
+                        borderRadius: BorderRadius.circular(5),
+                      ),
+                      child: Center(
+                        child: Icon(
+                          Icons.search,
+                          color: Colors.white,
+                          size: 25,
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              width: size.width,
+              margin: EdgeInsets.only(top: 20, left: 20),
+              child: Stack(
+                fit: StackFit.loose,
+                children: [
+                  Container(
+                    child: Text(
+                      'Category',
+                      style: TextStyle(
+                        color: Color(0xff363636),
+                        fontSize: 20,
+                        fontFamily: 'Roboto',
+                        fontWeight: FontWeight.w700,
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(right: 20, top: 1),
+                    child: Align(
+                      alignment: Alignment.centerRight,
+                      child: Text(
+                        'See all',
+                        style: TextStyle(
+                          color: Color(0xff5e5d5d),
+                          fontSize: 19,
+                          fontFamily: 'Roboto',
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              height: 120,
+              margin: EdgeInsets.only(top: 20, left: 20),
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: [
+                  demoCategories("assets/tooth.png", "Dentisty", "10 Doctors"),
+                  demoCategories("assets/brain.png", "Neurology", "5 Doctors"),
+                  demoCategories("assets/heart.png", "Dermatology", "4 Doctors"),
+                  demoCategories("assets/bone.png", "Orthopedics", "4 Doctors"),
+                  demoCategories("assets/bone.png", "Audilogy", "6 Doctors"),
+                  
+                ],
+              ),
+            ),
+             Container(
+              margin: EdgeInsets.only(top: 5, left: 20),
+              child: Text(
+                "About Us",
+                style: TextStyle(
+                  color: Color(0xff363636),
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Roboto',
+                ),
+              ),
+            ),
+             Container(
+              margin: EdgeInsets.only(top: 5, left: 20),
+              child: Text(
+                "using this app will save time for the patient to choose the suitable doctor for her desires and his/her location. So from our app, you can book a doctor appointment, schedule follow up teleconsultations, book doctors home visits, all at the click of a button",
+                style: TextStyle(
+                  color: Color(0xff363636),
+                  fontSize: 12,
+                  fontWeight: FontWeight.normal,
+                  fontFamily: 'Roboto',
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+//style color for the category of doctors
+  Widget demoCategories(String img, String name, String drCount) {
+    return Container(
+      width: 100,
+      margin: EdgeInsets.only(right: 15),
+      decoration: BoxDecoration(
+        color: Color(0xFF0077c6),//box color
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            child: Image.asset(img),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 10),
+            child: Text(
+              name,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 13,
+                fontFamily: 'Roboto',
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(top: 10),
+            padding: EdgeInsets.all(7),
+            decoration: BoxDecoration(
+              color: Color(0xffd9fffa).withOpacity(0.07),
+              borderRadius: BorderRadius.circular(5),
+            ),
+            child: Text(
+              drCount,
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 8,
+                fontFamily: 'Roboto',
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+}
