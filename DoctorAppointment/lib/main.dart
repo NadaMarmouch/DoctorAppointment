40e4f1171e12bd788421a8059ae6f850e57413dd
mariiamelbeheiry <mariam1806659@miuegypt.edu.eg>
2021-12-12 23:21:16 +0200

    doctor profile

--- DoctorAppointment/lib/main.dart
+++ DoctorAppointment/lib/main.dart
@@ -5,18 +5,21 @@ import 'screens/home_page.dart';
 import 'screens/doctor_details.dart';
 import 'screens/login.dart';
 import 'screens/editprofile.dart';
+import 'screens/profile.dart';
 import 'screens/first_screen.dart';
+import 'screens/doctor_profile.dart';
 
 void main() {
   runApp(MaterialApp(
     title: 'Doctor Appointment',
     routes: {
-      '/': (context) => firstscreen(),
-      '/signup': (context) => SignupPage(),
-      '/login': (context) => LoginPage(),
-      '/home': (context) => HomePage(),
-      '/details': (context) => DoctorDetailPage(),
-      '/editprofile': (context) => EditProfileUI(),
+      // '/': (context) => firstscreen(),
+      // '/signup': (context) => SignupPage(),
+      // '/login': (context) => LoginPage(),
+      // '/home': (context) => HomePage(),
+      // '/details': (context) => DoctorDetailPage(),
+      // '/editprofile': (context) => EditProfileUI(),
+      '/': (context) => DoctorProfilePage(),
     },
   ));
 }
