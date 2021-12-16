ca1c2e3291aea5ca63f4cd5c74641cfac714f628
NadaMarmouch <76530614+NadaMarmouch@users.noreply.github.com>
2021-12-16 16:10:14 +0200

    up

--- DoctorAppointment/lib/widget/doctor_name.dart
+++ DoctorAppointment/lib/widget/doctor_name.dart
@@ -16,57 +16,62 @@ class doctor_name extends StatelessWidget{
         onTap: () {
          Navigator.pushNamed(context, "/doctorreserve");
         },
-        child: Container(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal:10),
+          child: Container(
+            
        width: width,
-        decoration: BoxDecoration(
+          decoration: BoxDecoration(
        
-         color: Color(0xFF00abff),
-          borderRadius: BorderRadius.circular(20)
-        ),
-        padding: EdgeInsets.symmetric(horizontal:20,
-        vertical: 16),
-        child: Row(
-           mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Image.asset(img, height: 30,),
+           color: Color(0xFF00abff),
+            borderRadius: BorderRadius.circular(20)
+          ),
+          padding: EdgeInsets.symmetric(horizontal:20,
+          vertical: 16),
+      
+          child: Row(
+             mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Image.asset(img, height: 30,),
        SizedBox(width: 7,),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                Text(Name, style: TextStyle(
-                  color: Color(0xFF0077c6),
-                  fontSize: 14
-                ),),
-                SizedBox(height: 2,),
-                Text(categotry, style: TextStyle(
-                  fontSize: 14
-                ),),
-                 SizedBox(height: 2,),
-                 
-                Text(location, style: TextStyle(
-                  fontSize: 13
-                ),)
-              ],
-            ),
-              Container(
-              padding: EdgeInsets.symmetric(horizontal: 15,
-              vertical: 5),
-              margin: EdgeInsets.symmetric(horizontal: 20),
-              decoration: BoxDecoration(
-                color: Color(0xFF0077c6),
-                borderRadius: BorderRadius.circular(10)
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(Name, style: TextStyle(
+                    color: Color(0xFF0077c6),
+                    fontSize: 14
+                  ),),
+                  SizedBox(height: 2,),
+                  Text(categotry, style: TextStyle(
+                    fontSize: 14
+                  ),),
+                   SizedBox(height: 2,),
+                   
+                  Text(location, style: TextStyle(
+                    fontSize: 13
+                  ),)
+                ],
               ),
-              child: Text(fees, style: TextStyle(
-                
-                 color: Colors.white,
-                fontSize: 13,
-                fontWeight: FontWeight.w500
-          ),   ),),
-          
-             ],
-        ),
-   ) ); 
+                Container(
+                padding: EdgeInsets.symmetric(horizontal: 15,
+                vertical: 5),
+                margin: EdgeInsets.symmetric(horizontal: 20),
+                decoration: BoxDecoration(
+                  color: Color(0xFF0077c6),
+                  borderRadius: BorderRadius.circular(10)
+                ),
+                child: Text(fees, style: TextStyle(
+                  
+                   color: Colors.white,
+                  fontSize: 13,
+                  fontWeight: FontWeight.w500
+            ),   ),),
+            
+               ],
+          ),
+   ),
+        ) ); 
       
 }
 
